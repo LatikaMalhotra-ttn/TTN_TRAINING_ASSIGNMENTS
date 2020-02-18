58cb944e5a5ad08b646395d0e868a1425b5fbcca
latika malhotra <latika.malhotra@tothenew.com>
2020-02-17 20:28:47 -0800

    Ques 7 added

---
+++ Ques7_i/Ques7_i/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ques7_i
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 10;
+            Console.WriteLine(test);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
