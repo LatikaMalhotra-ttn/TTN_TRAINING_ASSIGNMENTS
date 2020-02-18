83f6c65130f26ace9552563cbdd55527ed9fe693
latika malhotra <latika.malhotra@tothenew.com>
2020-02-17 20:29:09 -0800

    Cont of Ques 7

---
+++ Ques7_ii/Ques7_ii/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim test As Integer = 10
+        Console.WriteLine(test)
+
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
